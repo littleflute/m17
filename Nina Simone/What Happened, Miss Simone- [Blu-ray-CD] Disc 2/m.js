ac484eb97637bffdf62ff983be3fbff395735251
littleflute <13699175164@sina.cn>
2017-06-27 18:22:24 -0500

    Add files via upload

---
+++ Nina Simone/What Happened, Miss Simone- [Blu-ray-CD] Disc 2/m.js
@@ -0,0 +1,17 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 I Loves You, Porgy.mp3";		
+f[2]	= "02 Don't Tell Me Be Misunderstood.mp3";		
+f[3]	= "03 I Put a Spell on You.mp3";		
+f[4]	= "04 Strange Fruit.mp3";		
+f[5]	= "05 Sinnerman.mp3";		
+f[6]	= "06 Mississippi Goodman.mp3";		
+f[7]	= "07 Little Girl Blue.mp3";		
+f[8]	= "08 Don't Smoke in Bed.mp3";		
+f[9]	= "09 My Baby Just Cares for Me.mp3";		
+f[10]	= "10 Lilac Wine.mp3";		
+f[11]	= "11 Black Is the Color of My True Love's Hair.mp3";		
+f[12]	= "12 Night Song.mp3";		
+f[13]	= "13 Nobody Knows You When You're Down and Out.mp3";		
+f[14]	= "14 Feeling Good.mp3";		
+f[15]	= "15 Ne Me Quitte Pas.mp3";		
